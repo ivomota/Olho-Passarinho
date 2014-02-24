bf00ea83ce2f5aa849a8f0fd62d463366bc51f84
Ivo Mota <ifvmota@gmail.com>
2014-02-24 18:54:45 +0000

    Adicionada pasta de com ficheiros de teste

---
+++ testes/insert_twitpic.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+location = 'data.db'
+table_name = 'IMAGENS'
+sql_file = './resultados/twitpic_urls.sql'
+
+conn = sqlite3.connect(location)
+c = conn.cursor()
+
+qry = open(sql_file, 'r').read()
+sqlite3.complete_statement(qry)
+
+c.executescript(qry)
+conn.commit()
+c.close()
+conn.close()
