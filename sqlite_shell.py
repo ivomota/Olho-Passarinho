018b1255585ef96e2b719acbda35ab12da69d84f
Ivo Mota <ifvmota@gmail.com>
2014-02-24 12:29:26 +0000

    Criado um sistema sheel para escrever a query + download imagens

---
+++ sqlite_shell.py
@@ -0,0 +1,34 @@
+# A minimal SQLite shell for experiments
+
+import sqlite3
+import get_images
+
+con = sqlite3.connect("data.db")
+con.isolation_level = None
+cur = con.cursor()
+
+buffer = ""
+
+print "Enter your SQL commands to execute in sqlite3."
+print "Enter a blank line to exit."
+
+while True:
+    line = raw_input()
+    if line == "":
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+
+            if buffer.lstrip().upper().startswith("SELECT"):
+                # print cur.fetchall()
+                qry = cur.fetchall()
+                print qry
+                get_images.get(qry)
+        except sqlite3.Error as e:
+            print "An error occurred:", e.args[0]
+        buffer = ""
+
+con.close()
