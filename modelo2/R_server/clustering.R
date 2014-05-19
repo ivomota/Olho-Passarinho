dd0ac08cd975aba944a25e35a2b5994ad48a5245
Ivo Mota <ifvmota@gmail.com>
2014-05-19 16:13:57 +0100

    Criação dos clusters para o primeiro sample

--- modelo2/R_server/clustering.R
+++ modelo2/R_server/clustering.R
@@ -3,32 +3,18 @@ setwd("~/Dropbox/FEUP/FEUP_13.14/Dissertacao/Olho-Passarinho/modelo2/R_server")
 set.seed(665544)
 x <- readRDS('../matrix_dist_norm/sample0_im_dist_norm.rds')
 
-# mean = mean(x)
-# std = sd(c(x))
-# l = length(x)
-# row = nrow(x)
-# col = ncol(x)
-# desv = (mean + (4*std))
-
-# new_x = matrix(data = NA, nrow = row, ncol = col, dimnames = NULL)
-
-# for (i in 1:row) {
-#   for (j in 1:col){
-#     n <- x[i, j]
-#     if(n > desv) { x[i,j] <- 0.0 }
-#   } 
-# }
-
 eps <- max(x)*0.02
-ds <- dbscan(x, eps, MinPts=2, method = "dist", showplot=1)
+ds <- dbscan(x, eps, MinPts=5, method = "dist", showplot=1)
 
 maxim = max(ds$cluster)
 len = length(ds$cluster)
 j = 0
 for (i in 0:maxim) {
-  nam <- paste("A", i, sep = "")
+  nam <- paste("C", i, sep = "")
   assign(nam, which(ds$cluster %in% i) )
-  nam <- paste("A", i, sep = "")
+  nam <- paste("S0", nam, sep = "_")
+  nam <- paste(nam, "txt", sep = ".")
+  nam <- paste("./clusters", nam, sep="/")
   write(which(ds$cluster %in% i), file=nam, ncolumns = len)
 }
 
