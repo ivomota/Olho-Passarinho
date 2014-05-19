7c2f970e947e9e2f6ecd80aae6d2ad201ddc2a5c
Ivo Mota <ifvmota@gmail.com>
2014-05-19 16:13:17 +0100

    Normalização das matrizes

--- modelo2/03ProcessImages/06normalize_matrix.py
+++ modelo2/03ProcessImages/06normalize_matrix.py
@@ -8,12 +8,6 @@ from rpy2.robjects.numpy2ri import numpy2ri
 
 def getmax(myarray):
 	return amax(myarray)
-	# test = []
-	# lenght = len(myarray[0])
-	# for r in xrange(lenght):
-	# 	test.append(max(myarray[r]))
-
-	# return max(test)
 
 def getmean(myarray):
 	return mean(myarray)
@@ -21,19 +15,19 @@ def getmean(myarray):
 def getstd(myarray):
 	return std(myarray)
 
-def norm(myarray, value_max):
+def norm(myarray, value_mean, value_std):
 	new_array = []
 	row = []
 	lenght = len(myarray[0])
 	for x in range(lenght):
 		for k in range(lenght):
 			value = myarray[x][k]
-			if value == 0:
+			if x >= k:
 				row.append(0)
 			else:
+				new_value = round((value - value_mean)/float(value_std), 3)
+				row.append(new_value)
 
-				row.append(round(value/float(value_max), 5))
-			
 		new_array.append(row)
 		row =[]
 	new_array = asarray(new_array)
@@ -46,7 +40,7 @@ def getfiles(path):
         
     return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('pkl')]
 
-path = './matrix_dist/'
+path = './matrix_im/'
 files = getfiles(path)
 
 for index, file in enumerate(files):
@@ -55,29 +49,31 @@ for index, file in enumerate(files):
 	l = len(matx)
 
 	# get the maximum value 
-	value_max = getmax(matx)
+	# value_max = getmax(matx)
 	value_mean = getmean(matx)
 	value_std = getstd(matx)
-
 
 	# print value_max
 	# print value_mean
 	# print value_std
-	print (value_max - value_mean)/ value_std
+	# print (value_max - value_mean)/ value_std
 
-	# mdist = norm(matx, value_max)
+	mdist = norm(matx, value_mean, value_std)
+	print mdist[0:9][0:9]
 
-	# transp = mdist.T
-	# print len(mdist)
-	# x1 = mdist.reshape((l,l))
-	# x2 = transp.reshape((l,l))
-	# mat = add(x1, x2)
-	# # print len(mat)
 
-	# mat = array(mat, dtype="float64") # <- convert to double precision numeric since R doesn't have unsigned ints
-	# ro = numpy2ri(mat)
-	# r.assign("bar", ro)
-	# r("saveRDS(bar, file='../matrix_dist_norm/sample"+ str(index) +"_im_dist_norm.rds')")
+	transp = mdist.T
+	print len(mdist)
+	x1 = mdist.reshape((l,l))
+	x2 = transp.reshape((l,l))
+	mat = add(x1, x2)
+	# print len(mat)
+	
+
+	mat = array(mat, dtype="float64") # <- convert to double precision numeric since R doesn't have unsigned ints
+	ro = numpy2ri(mat)
+	r.assign("bar", ro)
+	r("saveRDS(bar, file='../matrix_dist_norm/sample"+ str(index) +"_im_dist_norm.rds')")
 
 
 '''
